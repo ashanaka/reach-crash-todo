40b2156ea189cdb0efe89613f127c3af6b65c5e4
ashanaka <jpashanshanaka@gmail.com>
2021-05-10 15:18:21 +0530

    Routing initiated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Todos from "./components/Todos";
 import AddTodos from "./components/AddTodos";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 import React, { Component } from "react";
-import {v4 as uuid} from 'uuid';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 import "./App.css";
 
@@ -58,17 +60,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodos addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            delTodo={this.delTodo}
-            markComplete={this.markComplete}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodos addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    delTodo={this.delTodo}
+                    markComplete={this.markComplete}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>This is About page.</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde amet quasi
+        ex? Deserunt, repellat. Voluptates rerum at ex quos obcaecati nemo iusto
+        earum? Cum doloribus, minima asperiores odio dolor ullam.
+      </p>
+    </React.Fragment>
+  );
+}
